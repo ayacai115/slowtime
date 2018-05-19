50b0dd052daa865d20e82c6fce9b4eecc4c91390
ayacai115 <countrylife.uk@gmail.com>
2018-05-19 18:44:01 +0900

    footerにさるぼぼアイコン追加

--- footer.php
+++ footer.php
@@ -38,13 +38,13 @@
 <div class="access-wrapper container">
   <h3>ー　アクセス　ー</h3>
   <div class="access-wrapper-left col-xs-12 col-sm-6">
-    <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3218.3850906158673!2d137.1855199499188!3d36.23013830718361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6002a0e3aeac1725%3A0x737de4a6b8279b98!2z44GZ44KN44O844Gf44GE44KA!5e0!3m2!1sja!2sjp!4v1493459049652" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3218.3850906158673!2d137.1855199499188!3d36.23013830718361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6002a0e3aeac1725%3A0x737de4a6b8279b98!2z44GZ44KN44O844Gf44GE44KA!5e0!3m2!1sja!2sjp!4v1493459049652" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
   </div>
   <div class="access-wrapper-right col-xs-12 col-sm-6">
     <h5>薬草酵素温浴 Slow Time</h5>
     <p>〒509-4245 岐阜県飛騨市古川町幸栄町13-8<br>TEL (0577)73-2340｜FAX (0577)73-7717 </p>
     <p>ご予約 0120-78-2340(フリーダイヤル)</p>
-    <p style="color: red;">※ 4月1日(日)から営業時間が変更になります。</p>
+    <p style="color: red;">※ 2018年4月1日より営業時間が変更になりました。</p>
     <table style="margin: 0px; border: 0px;">
       <tr>
         営業時間
@@ -58,6 +58,10 @@
     <p>年末年始、古川祭(4月19日・20日)、ゴールデンウィーク、お盆は休業いたします。</br>
     そのほか慶事・弔事や天候の影響などによる臨時休業の場合もございます。</br>
     お知らせは<a href="https://www.facebook.com/slowtime">Facebook</a>に投稿しておりますので、詳しくはそちらをごらんください。</p>
+    <div class="sarubobo-wrapper">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.hidashin.co.jp/coin/"><img src="<?php echo esc_url( home_url( '/wp-content/themes/slowtime/img/sarubobo.png' ) ); ?>" title="さるぼぼコイン(外部リンクです)" width="100"></a>
+      <p>さるぼぼコイン使えます。</p>
+    </div>
   </div>
 </div>
 
